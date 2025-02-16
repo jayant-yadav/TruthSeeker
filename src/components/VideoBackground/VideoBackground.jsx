5f80dab941fd07b3a7ed0c0781fc988266773ea6
MarcusOkodugha <100699584+MarcusOkodugha@users.noreply.github.com>
2025-02-16 15:44:37 +0100

    muted with auto play

--- src/components/VideoBackground/VideoBackground.jsx
+++ src/components/VideoBackground/VideoBackground.jsx
@@ -3,7 +3,7 @@ import './VideoBackground.css'
 function VideoBackground() {
   return (
     <div className="video-container">
-      <video autoPlay loop className='video'>
+      <video autoPlay muted loop className='video'>
         <source src="/video4.mp4" type="video/mp4" />
       </video>
     </div>
